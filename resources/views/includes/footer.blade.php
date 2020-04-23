e55fc534d9ead9673e39825042e9eccde9d90bcc
rakaadli <34180704+rakaadli@users.noreply.github.com>
2020-04-23 20:44:41 +0700

    second commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,54 @@
+
+  <footer class="section-footer mt-5 mb-4 border-top">
+    <div class="container pt-5 pb-5">
+      <div class="row justify-content-center">
+        <div class="col-12">
+          <div class="row">
+            <div class="col-12 col-lg-3">
+              <h5>Features</h5>
+              <ul class="list-unstyled">
+                <li><a href="#">Reviews</a></li>
+                <li><a href="#"></a>Comunity</li>
+                <li><a href="#"></a>Social Media Kit</li>
+                <li><a href="#"></a>Affiliate</li>
+              </ul>
+            </div>
+            <div class="col-12 col-lg-3">
+              <h5>ACCOUNT</h5>
+              <ul class="list-unstyled">
+                <li><a href="#">Refund</a></li>
+                <li><a href="#"></a>Security</li>
+                <li><a href="#"></a>Rewards</li>
+
+              </ul>
+            </div>
+            <div class="col-12 col-lg-3">
+              <h5>COMPANY</h5>
+              <ul class="list-unstyled">
+                <li><a href="#">Career</a></li>
+                <li><a href="#"></a>Help Center</li>
+                <li><a href="#"></a>Media</li>
+
+              </ul>
+            </div>
+            <div class="col-12 col-lg-3">
+              <h5>GET CONNECTED</h5>
+              <ul class="list-unstyled">
+                <li><a href="#">Jakarta Timur</a></li>
+                <li><a href="#"></a>Indonesia</li>
+                <li><a href="#"></a>0878-8580-4646</li>
+                <li><a href="#"></a>support@snaptravel.com</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row border-top justify-content-center pt-4">
+        <div class="col-auto text-gray-500 font-weitgh-light">
+          2020 Copyright Snaptravel All Right Reserved Made in Jakarta
+        </div>
+      </div>
+    </div>
+  </footer>
